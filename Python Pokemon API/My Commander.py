d948810f71802dee441bb30041d151db6c107a5b
7Junior-Code <aktan.kasymaliev2014@gmail.com>
2024-12-14 10:18:34 +0400

    Add files via upload

---
+++ Python Pokemon API/My Commander.py
@@ -0,0 +1,26 @@
+# API using Python
+from http.client import responses
+
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+manual = input("Enter your pokemon: ").lower()
+pokemon_name = f"{manual}"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"].capitalize()}")
+    print(f"ID: {pokemon_info["id"]}")
+    print(f"Height: {pokemon_info["height"]}")
+    print(f"Weight: {pokemon_info["weight"]}")
